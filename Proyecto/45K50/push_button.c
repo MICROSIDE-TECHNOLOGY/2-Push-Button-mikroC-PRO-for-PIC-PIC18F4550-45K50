b36d1d543fb462fb23b8ff536755836b0e212d81
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:27:35 -0600

    Fixed identation

--- Proyecto/45K50/push_button.c
+++ Proyecto/45K50/push_button.c
@@ -16,28 +16,26 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF           //Espacio reservado para bootloader
+#pragma orgall 0x1FFF         // Espacio reservado para bootloader
 
-#define LED_ON LATA4_bit = 1    //Se declara una definición que asigna el pin y su estado lógico
-#define LED_OFF LATA4_bit = 0   //Se declara una definición que asigna el pin y su estado lógico
-                                //Tome en cuenta que en versiones anteriores se hacia uso del pin A1
-                                
-#define Boton PORTA.f2          //Se declara una definición que devuelve el estado lógico de un pin configurado como entrada digital
+#define LED_ON LATA4_bit = 1  // Se declara una definición que asigna el pin y su estado lógico
+#define LED_OFF LATA4_bit = 0 // Se declara una definición que asigna el pin y su estado lógico
+                              // Tome en cuenta que en versiones anteriores se hacia uso del pin A1
 
-void main() org 0x2000          //Se instancia el vector de inicio usando bootloader MICROSIDE
+#define Boton PORTA.f2        // Se declara una definición que devuelve el estado lógico de un pin configurado como entrada digital
+
+void main() org 0x2000        // Se instancia el vector de inicio usando bootloader MICROSIDE
 {
-  LATA4_bit = 0;                //Escribe ceros a la salida del pin A4
-  TRISA4_bit = 0;               //Se declara como un pin de salida
-  TRISA2_bit = 1;               //Se declara como un pin de entrada
-  ANSELA = 0;
+   LATA4_bit = 0;             // Escribe ceros a la salida del pin A4
+   TRISA4_bit = 0;            // Se declara como un pin de salida
+   TRISA2_bit = 1;            // Se declara como un pin de entrada
+   ANSELA = 0;
 
-  while(1)
-  {
-     if (Boton==1) {             //Pregunta el estado del boton
-        LED_OFF;                 //Apaga el LED
-     } else {
-        LED_ON;                  //Enciende el LED
-     }
-  }
-
+   while ( 1 ) {
+      if ( Boton == 1 ) {     // Pregunta el estado del boton
+         LED_OFF;             // Apaga el LED
+      } else {
+         LED_ON;              // Enciende el LED
+      }
+   }
 }
